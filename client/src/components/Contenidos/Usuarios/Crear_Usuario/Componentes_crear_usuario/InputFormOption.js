095c4fffcdb7a49da9c640b6381f39b0fab56e04
agamboar <gamboaalvaro96@gmail.com>
2021-07-29 23:16:54 -0400

    fix usuario responsive

--- client/src/components/Contenidos/Usuarios/Crear_Usuario/Componentes_crear_usuario/InputFormOption.js
+++ client/src/components/Contenidos/Usuarios/Crear_Usuario/Componentes_crear_usuario/InputFormOption.js
@@ -9,10 +9,10 @@ export default class Contenido_Agente_Aduana extends Component {
         return (
             <div className="col-6 mb-2">
                 <div className="row">
-                    <div className="col-6">
+                    <div className="col-4">
                         <label className="input-group-text ancho2 rounded-pill " for="inputGroupSelect01">Rol</label>
                     </div>
-                    <div className="col-8">
+                    <div className="col-6">
                         <select className="form-select ancho alto"  id="inputGroupSelect01" value = {this.props.rol} onChange={this.props.onChange} >
                           <option defaultValue>Escoger el Rol del Usuario</option>
                           <option value={1}>Administrador</option>
